d88bbfd9023740018487cb8cc0694e938cd03130
katherine <katherinebemol17@gmail.com>
2023-04-12 14:54:49 -0500

    last changes

--- src/CatchBridsApp.js
+++ src/CatchBridsApp.js
@@ -1,12 +1,26 @@
 import './App.css';
 import React from 'react';
-import { Container, Row, Col } from 'react-grid-system';
+import { Container} from 'react-grid-system';
 import aveAzul from "./images/aveAzul.png";
 import aveRosa from "./images/aveRosa.png";
 import Card from './Card';
-import Header from './Header';
+import { useState } from "react"
 
 function App() {
+
+  // Código para el contador
+  const [contsRosas, setContRosas] = useState(0);
+	const [contsAzules, setContAzules] = useState(0);
+	
+  const countBirds = (numero) => {
+    const idAve = numero % 2 === 0 ? aveRosa : aveAzul;
+    if(idAve === aveRosa){
+      setContRosas(contsRosas +1)
+    } else {
+      setContAzules(contsAzules +1)
+    }
+  }
+
 
   const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
@@ -15,15 +29,36 @@ function App() {
     const ave = esPar ? aveRosa : aveAzul;
 
     return (
-      <>
-        <Card bird = {ave}></Card>
-      </>
+
+      <Card 
+        key={numero}
+        onClick={countBirds} 
+        bird= {ave}
+      />
+   
     );
   });
 
+  
   return (
     <>
-    <Header></Header>
+    <header className="">
+			<div>
+				<p>Aves rosas</p>
+				<p>{contsRosas}</p>
+			</div>
+
+			<div>
+				<p>Aves azules</p>
+				<p>{contsAzules}</p>
+			</div>
+
+			<div>
+				<p>Tiempo restante</p>
+				<p></p>
+			</div>
+		</header>
+
     <Container className='box-container'>
       {cartas}
     </Container>
